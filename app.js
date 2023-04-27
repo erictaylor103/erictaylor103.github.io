1ad8a02dabc785bcaa93d0de35ff5cddfa78ee14
Eric Taylor <etaylor@Erics-MacBook-Pro-2.local>
2023-04-27 17:24:03 -0400

    adds new nav bar and updates all styles

--- app.js
+++ app.js
@@ -1,26 +1,9 @@
-const navSlide = () => {
-    const burger  = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-
-    burger.addEventListener('click', ()=>{
-        //toggle nav
-        nav.classList.toggle('nav-active');
-        
-        //animate links
-        navLinks.forEach((link, index)=>{
-            if(link.style.animation){
-                link.style.animation = '';
-            }else{
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-            }
-        });
-
-        //burger animation
-        burger.classList.toggle('toggle');
-    });
-
-    
-}
-
-navSlide();
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
